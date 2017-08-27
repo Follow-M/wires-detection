5bd2027fc00937f61b7ada7b3a8a2836bd56c92a
Olya-Kopach <olya.kopach@gmail.com>
2017-08-27 18:26:53 +0300

    threshold operation for selecting wires among all lines

--- opencvTry/Source.cpp
+++ opencvTry/Source.cpp
@@ -7,7 +7,8 @@
 using namespace cv;
 using namespace std;
 
-Mat src, mask;
+Mat src, src1, mask;
+Mat inpaintMask;
 vector<Vec4i> lines;
 Point mouse_position;
 
@@ -22,7 +23,7 @@ int main(int argc, char** argv) {
 
 
 	src = imread("1.jpg", IMREAD_COLOR);
-	Mat src1 = imread("1.jpg", IMREAD_COLOR);
+	src1 = imread("1.jpg", IMREAD_COLOR);
 	if (src.empty())
 	{
 		cerr << "No image supplied" << endl;
@@ -30,7 +31,9 @@ int main(int argc, char** argv) {
 	}
 	GaussianBlur(src, src, Size(3, 3), 0, 0, BORDER_DEFAULT);
 	namedWindow("Initial photo", WINDOW_KEEPRATIO);
-	imshow("Initial photo", src);
+	imshow("Initial photo", src1);
+	namedWindow("Initial photo+b", WINDOW_KEEPRATIO);
+	imshow("Initial photo+b", src);
 	waitKey(0);
 
 	Mat src_closed, result_gray, binary, binary1;
@@ -65,35 +68,33 @@ int main(int argc, char** argv) {
 
 	waitKey(0);
 
-	Mat inpaintMask = Mat::zeros(src.size(), CV_8U);
+	inpaintMask = Mat::zeros(src.size(), CV_8U);
 	Mat res;
-
-		mask = Mat::zeros(src.size(), CV_8U);
 
 	HoughLinesP(binary1, lines, 1, CV_PI / 180, 100, 30, 4);
 	for (size_t i = 0; i < lines.size(); i++)
 	{
-
+		mask = Mat::zeros(src.size(), CV_8U);
 		line(mask, Point(lines[i][0], lines[i][1]), Point(lines[i][2], lines[i][3]), Scalar(255, 255, 255), 1, CV_AA);
-		/*if (!backgr_check(i))
+		if (!backgr_check(i))
 		{
 			line(src, Point(lines[i][0], lines[i][1]), Point(lines[i][2], lines[i][3]), Scalar(0, 0, 255), 1, CV_AA);
 			line(inpaintMask, Point(lines[i][0], lines[i][1]), Point(lines[i][2], lines[i][3]), Scalar(255, 255, 255), 1, CV_AA);
-		}*/
-
-
+		}
 	}
 
+	//TODO: transform inpaintMask so only approved lines are there
+
 	/// A base algorithm, naturally should be called right after backgr_check
-	namedWindow("Please submit wires to delete", WINDOW_KEEPRATIO);
-	imshow("Please submit wires to delete", mask);
+	/*namedWindow("Please submit wires to delete", WINDOW_KEEPRATIO);
+	imshow("Please submit wires to delete", inpaintMask);
 	waitKey(0);
 	setMouseCallback("Please submit wires to delete", mouse_callback, NULL);
 	waitKey(0);
 	for (size_t i = 0; i < lines.size(); i++)
 	{
 		submit_wires(Point(lines[i][0], lines[i][1]), Point(lines[i][2], lines[i][3]));
-	}
+	}*/
 
 
 
@@ -103,7 +104,11 @@ int main(int argc, char** argv) {
 
 	waitKey(0);
 
-	inpaint(src1, inpaintMask, src1, 5, INPAINT_TELEA);
+	Mat elem = getStructuringElement(MORPH_RECT, Size(2, 2), Point(-1, -1));
+	Mat dilated;
+	dilate(inpaintMask, dilated, elem);
+
+	inpaint(src1, dilated, src1, 5, INPAINT_TELEA);
 
 	namedWindow("r", WINDOW_KEEPRATIO);
 	imshow("r", src1);
@@ -154,7 +159,7 @@ int backgr_check(int num)
 int count_av_color(int x, int y)
 {
 	//mask - one line we are working with
-	Mat init = src = imread("1.jpg", IMREAD_COLOR);
+	//Mat init = src = imread("1.jpg", IMREAD_COLOR);
 
 	Mat elem = getStructuringElement(MORPH_RECT, Size(1, 1), Point(-1, -1));
 	Mat dilated;
@@ -169,6 +174,9 @@ int count_av_color(int x, int y)
 	bool line = false, change_diff = false, change_same = false, first_array = true;
 
 	int i, j;
+
+	int threshold = 90, color_variance;
+
 
 	for (j = y - kernel; j <= y + kernel; j++)
 	{
@@ -267,9 +275,12 @@ int count_av_color(int x, int y)
 	m2.y /= m2Count;
 	m2.z /= m2Count;
 
-	//add checking with threshold
+	color_variance = (int)(pow(m1.x - m2.x, 2) + pow(m1.y - m2.y, 2) + pow(m1.z - m2.z, 2));
 
-	return 1;
+	if (color_variance <= threshold)
+		return 1;
+	else
+		return 0;
 }
 
 
@@ -284,9 +295,9 @@ void mouse_callback(int event, int x, int y, int flags, void* userdata) {
 bool submit_wires(Point line_begin, Point line_end) {
 
 	if ((line_begin.x < 0) ||
-		(line_begin.x > mask.rows) ||
+		(line_begin.x > inpaintMask.rows) ||
 		(line_begin.y < 0) ||
-		(line_begin.y > mask.cols)){
+		(line_begin.y > inpaintMask.cols)){
 		cout << "\n__________________\n";
 		cout << "_________OUT OF IMAGE RANGE___________";
 		cout << "\n__________________\n";
@@ -294,9 +305,9 @@ bool submit_wires(Point line_begin, Point line_end) {
 	}
 
 	if ((line_end.x < 0) ||
-		(line_end.x > mask.rows) ||
+		(line_end.x > inpaintMask.rows) ||
 		(line_end.y < 0) ||
-		(line_end.y > mask.cols)){
+		(line_end.y > inpaintMask.cols)){
 		cout << "\n__________________\n";
 		cout << "_________OUT OF IMAGE RANGE___________";
 		cout << "\n__________________\n";
@@ -308,9 +319,9 @@ bool submit_wires(Point line_begin, Point line_end) {
 		for (int j = line_begin.x - eps; j < line_begin.x + eps; ++j) {
 
 			if ((j <= 0) ||
-				(j >= mask.cols - (eps+1)) ||
+				(j >= inpaintMask.cols - (eps+1)) ||
 				(i <= 0) ||
-				(i >= mask.rows - (eps+1))){
+				(i >= inpaintMask.rows - (eps+1))){
 				cout << "\n__________________\n";
 				cout << "OUT OF IMAGE RANGE";
 				cout << "\n__________________\n";
@@ -318,8 +329,8 @@ bool submit_wires(Point line_begin, Point line_end) {
 			}
 
 			cout << "i=" << i << " j=" << j << "\nmouse_position_x=" << mouse_position.x << " mouse_position_y=" << mouse_position.y << "\nline_begin_y=" << line_begin.y << " line_begin_x" << line_begin.x << endl;
-			mask.at<uchar>(i, j) = 200; /// check which pixel is operated
-			imshow("Please submit wires to delete", mask);
+			inpaintMask.at<uchar>(i, j) = 200; /// check which pixel is operated
+			imshow("Please submit wires to delete", inpaintMask);
 			waitKey(1);
 
 			if ((i == mouse_position.y) &&
@@ -332,17 +343,17 @@ bool submit_wires(Point line_begin, Point line_end) {
 		for (int j = line_end.x - eps; j < line_end.x + eps; ++j) {
 
 			if ((j <= 0) ||
-				(j >= mask.cols - (eps+1)) ||
+				(j >= inpaintMask.cols - (eps+1)) ||
 				(i <= 0) ||
-				(i >= mask.rows - (eps+1))){
+				(i >= inpaintMask.rows - (eps+1))){
 				cout << "\n__________________\n";
 				cout << "OUT OF IMAGE RANGE";
 				cout << "\n__________________\n";
 				continue;
 			}
 			cout << "i=" << i << " j=" << j << "\nmouse_position_x=" << mouse_position.x << " mouse_position_y=" << mouse_position.y << "\nline_end_y=" << line_begin.y << " line_end_x" << line_begin.x << endl;
-			mask.at<uchar>(i, j) = 200; /// check which pixel is operated
-			imshow("Please submit wires to delete", mask);
+			inpaintMask.at<uchar>(i, j) = 200; /// check which pixel is operated
+			imshow("Please submit wires to delete", inpaintMask);
 			waitKey(1);
 
 			if ((i == mouse_position.y) &&
